636a895a2e67ffe7e6352577a38ec9c196e37f03
nextbendev <gastronomicllc@gmail.com>
2019-06-27 12:54:20 -0400

    bknd

---
+++ assets/js/register.js
@@ -0,0 +1,12 @@
+﻿(function() {
+
+    $('#continue').on('click',function() {
+        $('#step-1').addClass('hidden');
+        $('#step-2').removeClass('hidden');
+    });
+
+    $('#back').on('click', function () {
+        $('#step-2').addClass('hidden');
+        $('#step-1').removeClass('hidden');
+    });
+})();
